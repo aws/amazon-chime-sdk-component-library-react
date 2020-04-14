448e956deba2ab4ae203daead5fe4039a472ec1d
Karen Guo <xixia@amazon.com>
2020-04-14 10:20:25 -0700

    Add react base app as a starting point for demo app

---
+++ demo/react/src/app.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import MeetingForm from './containers/MeetingForm';
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MeetingForm />
+      </div>
+    );
+  }
+}
+
+export default App;
