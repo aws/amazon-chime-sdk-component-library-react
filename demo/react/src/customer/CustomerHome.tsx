fad52f249e530f0f28139a917f560b8c027ee32c
Karen Guo <xixia@amazon.com>
2020-04-15 10:42:46 -0700

    Add router for customer and agent

---
+++ demo/react/src/customer/CustomerHome.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import MeetingForm from './MeetingForm';
+
+const CustomerHome = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Do you have a question?</h1>
+      <MeetingForm />
+    </div>
+  );
+};
+
+export default CustomerHome;
