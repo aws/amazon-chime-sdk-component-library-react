6fbceac09a045429995eb96e95895c782bb1909e
Peyton Foucht <pfoucht@amazon.com>
2020-04-16 08:50:55 -0700

    Add base VideoGrid component

---
+++ src/components/VideoGrid/VideoGrid.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { number } from '@storybook/addon-knobs';
+
+import VideoGrid from './';
+
+const Tile = () => (
+  <div
+    style={{
+      padding: '1rem',
+      border: '1px solid #212020',
+      backgroundColor: '#424242',
+      color: 'white',
+    }}
+  >
+    Tile
+  </div>
+);
+
+storiesOf('VideoGrid', module).add('VideoGrid', () => {
+  const size = number('Size', 3);
+  const tiles = new Array(size).fill(0).map((x, index) => <Tile key={index} />);
+
+  return (
+    <div style={{ padding: '1rem', height: '100vh', boxSizing: 'border-box' }}>
+      <VideoGrid>{tiles}</VideoGrid>
+    </div>
+  );
+});
