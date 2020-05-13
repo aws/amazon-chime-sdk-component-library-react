8ba4aea6c0d20f77482f79dbbebf70998606bc8e
Karen Guo <xixia@amazon.com>
2020-05-13 14:21:38 -0700

    Fix typo in demo app

--- demo/meeting/src/index.tsx
+++ demo/meeting/src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import App from './App';
+import App from './app';
 
 window.addEventListener('load', () => {
   ReactDOM.render(<App />, document.getElementById('root'));
