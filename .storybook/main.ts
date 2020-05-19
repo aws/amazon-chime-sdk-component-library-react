248a116b8e1e236fc3d30c4843460ac4448601ac
Dave Lane <landave@amazon.com>
2020-05-19 10:05:42 -0700

    Include storybok viewport addon for responsive views

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-knobs/register',
     '@storybook/addon-a11y/register',
     '@storybook/addon-storysource/register',
+    '@storybook/addon-viewport/register',
     {
       name: '@storybook/addon-docs',
       options: {
