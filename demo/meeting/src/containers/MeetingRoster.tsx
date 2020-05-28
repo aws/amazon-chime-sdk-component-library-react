49291cf2a551edf2d7f3f2d5c4491d13028061b1
Venkatesh Devale <devalevd@amazon.com>
2020-05-27 20:06:05 -0700

    Add meeting roster

---
+++ demo/meeting/src/containers/MeetingRoster.tsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+
+import { getRosterContext } from '../providers/RosterProvider';
+import RosterItem from './RosterItem';
+import useActiveSpeakerDetector from '../hooks/useActiveSpeakerDetector';
+
+const StyledRoster = styled.div`
+  border: 1px solid;
+  display: inline-flex;
+  flex-direction: column;
+  position: fixed;
+  right: 1rem;
+  top: 3.5rem;
+`;
+
+const MeetingRoster = () => {
+  const roster = useContext(getRosterContext());
+  const activeSpeakerAttendee = useActiveSpeakerDetector();
+  const attendees = Object.values(roster).map(item => {
+    const { id, name } = item;
+    return (
+      <RosterItem
+        key={id}
+        id={id}
+        name={name}
+        isActiveSpeaker={activeSpeakerAttendee === id}
+      />
+    );
+  });
+  return <StyledRoster>{attendees}</StyledRoster>;
+};
+
+export default MeetingRoster;
