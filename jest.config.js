83acd76846dc6fdc32734b905058391eeb312298
Ziyi Zhang <ziyiz@amazon.com>
2020-06-12 02:33:15 -0700

    Run visual regression tests in Docker to avoid pixels mismatch in different running environments

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    preset: 'jest-puppeteer',
     testRegex: './*\\.test\\.tsx$',
     setupFilesAfterEnv: ['./tst/setupTests.js'],
     moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx", "node",],
