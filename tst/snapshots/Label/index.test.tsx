c963b0405c98fa5636decea9d26ac43e783052c8
Brad McKnight <bradman@amazon.com>
2020-06-19 13:11:31 -0700

    Update label snapshot test

--- tst/snapshots/Label/index.test.tsx
+++ tst/snapshots/Label/index.test.tsx
@@ -1,6 +1,6 @@
 describe('Label', () => {
   it('label', async () => {
-    await page.goto('http://host.docker.internal:9009/iframe.html?id=form-label--label');
+    await page.goto('http://host.docker.internal:9009/iframe.html?id=label--basic-label');
     const image = await page.screenshot();
     expect(image).toMatchImageSnapshot();
   });
