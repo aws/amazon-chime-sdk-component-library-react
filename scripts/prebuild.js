7e7aa11e77c567d8417071a1350f80efbabbcba8
Nikolay Kravchuk <nikk@amazon.com>
2020-06-29 12:01:34 -0700

    Test actions

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -17,8 +17,8 @@ function verbose(command) {
 
 const base = fs.readFileSync(path.join(process.cwd(), '.base-branch'), 'utf8').trim();
 
-console.log(`Is GITHUB_ACTIONS: ${GITHUB_ACTIONS}`);
-const commits = exec(`git rev-list ${base}..`).toString().trim().split(`\n`);
+// console.log(`Is GITHUB_ACTIONS: ${GITHUB_ACTIONS}`);
+const commits = exec(`git rev-list HEAD ^${base}`).toString().trim().split(`\n`);
 console.log(`Commits: ${commits}`);
 let commit_files = [];
 
