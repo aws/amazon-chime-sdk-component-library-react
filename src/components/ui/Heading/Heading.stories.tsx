fef23f5425a7f8cb9fb2ff5d45e0545f79abfb71
Ziyi Zhang <ziyiz@amazon.com>
2020-07-20 12:46:52 -0700

    Add docs for Heading and ControlBar components

--- src/components/ui/Heading/Heading.stories.tsx
+++ src/components/ui/Heading/Heading.stories.tsx
@@ -6,16 +6,23 @@ import { select, text } from '@storybook/addon-knobs';
 
 import Flex from '../Flex';
 import Heading from './';
+import HeadingDocs from './Heading.mdx';
 
 export default {
   title: 'UI Components/Headings',
+  parameters: {
+    docs: {
+      page: HeadingDocs.parameters.docs.page().props.children.type
+    }
+  },
+  component: Heading
 };
 
 export const BasicHeading = () => (
-  <Flex layout="fill-space-centered" style={{ "width": "auto" }}>
+  <Flex layout="fill-space-centered" style={{ width: 'auto' }}>
     <Heading
       css={text('css', 'color: palevioletred;')}
-      level={select('level', {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6 }, 1)}
+      level={select('level', { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6 }, 1)}
       tag={text('tag', 'p')}
     >
       Change my <code>level</code> or <code>as</code> prop
@@ -24,5 +31,5 @@ export const BasicHeading = () => (
 );
 
 BasicHeading.story = {
-  name: 'Basic heading',
+  name: 'Basic heading'
 };
