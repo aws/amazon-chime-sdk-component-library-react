725e124d0b892976c2b5a12ad14d64d182c3669b
Brad McKnight <bradman@amazon.com>
2020-10-12 15:29:49 -0700

    Changing ChannelList props to accept any children instead of PopOverItem props

--- src/components/ui/Chat/ChannelList/index.tsx
+++ src/components/ui/Chat/ChannelList/index.tsx
@@ -1,23 +1,24 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { HTMLAttributes } from 'react';
+import React, { FC, HTMLAttributes } from 'react';
 
 import { ChannelListContext } from './ChannelListContext';
 import { StyledChannelList } from './Styled';
 import { BaseProps } from '../../Base';
+import { ChannelItemProps } from './ChannelItem';
 
 export interface ChannelListProps
   extends Omit<HTMLAttributes<HTMLUListElement & HTMLLIElement>, 'css'>,
     BaseProps {
-      activeChannelArn: string;
-      setActiveChannelArn: (arn: string) => void;
+      activeChannelId: string;
+      setActiveChannelId: (id: string) => void;
 }
 
-export const ChannelList = (props: ChannelListProps) => {
-  const { activeChannelArn, setActiveChannelArn } = props;
+export const ChannelList: FC<ChannelListProps> = (props) => {
+  const { activeChannelId, setActiveChannelId } = props;
 
-  const channelListContext = { setActiveChannelArn, activeChannelArn };
+  const channelListContext = { setActiveChannelId, activeChannelId };
 
   return (
     <ChannelListContext.Provider value={channelListContext}>
--- tst/components/ui/Chat/ChannelList/ChannelItem.test.tsx
+++ tst/components/ui/Chat/ChannelList/ChannelItem.test.tsx
@@ -6,46 +6,47 @@ import React from 'react';
 
 import ChannelList from '../../../../../src/components/ui/Chat/ChannelList';
 import ChannelItem from '../../../../../src/components/ui/Chat/ChannelList/ChannelItem';
+import { PopOverItem } from '../../../../../src/components/ui/PopOver/PopOverItem';
 import lightTheme from '../../../../../src/theme/light'
 import { renderWithTheme } from '../../../../test-helpers';
 
 describe('ChannelItem', () => {
 
   it('renders a ChannelItem', () => {
-    const component = <ChannelItem name='Channel 1' arn='abc' />;
+    const component = <ChannelItem name='Channel 1' id='abc' />;
     const { getByText } = renderWithTheme(lightTheme, component)
     const el = getByText('Channel 1');
     expect(el).toBeInTheDocument();
   });
 
-  it('should render a more options button if more options are provided and the channel is active is the list', () => {
+  it('should render a channel actions button if more options are provided and the channel is active is the list', () => {
     const component = (
-      <ChannelList activeChannelArn='abc' setActiveChannelArn={() => null}>
-        <ChannelItem name='Channel 1' arn='abc' moreOptions={[{ children: <span>Option 1</span>, onClick: () => null }]} />
+      <ChannelList activeChannelId='abc' setActiveChannelId={() => null}>
+        <ChannelItem name='Channel 1' id='abc' actions={<PopOverItem children={<span>Option 1</span>} onClick={() => console.log('Option 1 clicked')} />} />
       </ChannelList>
     );
     const { getByTestId } = renderWithTheme(lightTheme, component)
-    const el = getByTestId('more-channel-options');
+    const el = getByTestId('channel-actions');
     expect(el).toBeInTheDocument();
   });
 
-  it('should not render a more options button if more options are not provided', () => {
+  it('should not render a channel actions button if more options are not provided', () => {
     const component = (
-        <ChannelList activeChannelArn='abc' setActiveChannelArn={() => null}>
-          <ChannelItem name='Channel 1' arn='abc' />
+        <ChannelList activeChannelId='abc' setActiveChannelId={() => null}>
+          <ChannelItem name='Channel 1' id='abc' />
         </ChannelList>
       );    const { queryByTestId } = renderWithTheme(lightTheme, component)
-    const el = queryByTestId('more-channel-options');
+    const el = queryByTestId('channel-actions');
     expect(el).not.toBeInTheDocument();
   });
 
   it('should not render a more options button if the channel is not active', () => {
     const component = (
-        <ChannelList activeChannelArn='cde' setActiveChannelArn={() => null}>
-          <ChannelItem name='Channel 1' arn='abc' moreOptions={[{ children: <span>Option 1</span>, onClick: () => null }]} />
+        <ChannelList activeChannelId='cde' setActiveChannelId={() => null}>
+          <ChannelItem name='Channel 1' id='abc' actions={<PopOverItem children={<span>Option 1</span>} onClick={() => console.log('Option 1 clicked')} />} />
         </ChannelList>
       );    const { queryByTestId } = renderWithTheme(lightTheme, component)
-    const el = queryByTestId('more-channel-options');
+    const el = queryByTestId('channel-actions');
     expect(el).not.toBeInTheDocument();
   });
 });
