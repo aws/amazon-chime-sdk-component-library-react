cc42f469b96704af11d4e9a98f60b51b28bd8ed4
Jingyi Ren <renjingy@amazon.com>
2020-11-12 06:35:23 -0800

    Add Dock/Attachment/Emoji icons and polish PopOver menu style (#284)

--- src/components/ui/icons/index.tsx
+++ src/components/ui/icons/index.tsx
@@ -20,8 +20,10 @@ export { default as DeskPhone } from './DeskPhone';
 export { default as Dialer } from './Dialer';
 export { default as Dislike } from './Dislike';
 export { default as Dots } from './Dots';
+export { default as Dock } from './Dock';
 export { default as Document } from './Document';
 export { default as Eye } from './Eye';
+export { default as EmojiPicker } from './EmojiPicker';
 export { default as Feedback } from './Feedback'
 export { default as Hamburger } from './Hamburger';
 export { default as Information } from './Information';
