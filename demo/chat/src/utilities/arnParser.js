34850b2ca24e09bb04d34f9f0b8323c8e35f37bd
Nikolay Kravchuk <nikk@amazon.com>
2020-11-24 15:41:40 -0800

    Add Chat Demo App

---
+++ demo/chat/src/utilities/arnParser.js
@@ -0,0 +1,20 @@
+const arnParser = (arn) => {
+  // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+  // SPDX-License-Identifier: Apache-2.0
+
+  const arnMap = [
+    'arn',
+    'aws',
+    'service',
+    'region',
+    'namespace',
+    'relativeId',
+    'relativeValue',
+  ];
+  return arn.split(':').reduce(function (aggregator, piece, index) {
+    aggregator[arnMap[index]] = piece;
+    return aggregator;
+  }, {});
+};
+
+export default arnParser;
