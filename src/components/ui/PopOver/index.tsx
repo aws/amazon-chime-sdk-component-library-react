31932d56de7df2775350e0a4b920048ea0b5e1d7
Nicole Wang <wangnng@amazon.com>
2021-01-07 08:26:13 -0800

    Add className to <StyledPopOverMenu> in order to give styling access

--- src/components/ui/PopOver/index.tsx
+++ src/components/ui/PopOver/index.tsx
@@ -160,6 +160,7 @@ export const PopOver: FC<PopOverProps> = ({
                 ref={ref}
                 style={style}
                 data-testid="menu"
+                className="ch-popover-menu"
               >
                 {children}
               </StyledPopOverMenu>
