189e6b758edec3fd97cc2f95b7311a941766854d
Brad McKnight <bradman@amazon.com>
2021-01-07 16:12:26 -0800

    Export useDevicePermissionStatus from library

--- demo/meeting/src/enums/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2020-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-export enum DevicePermissionStatus {
-  UNSET = 'UNSET',
-  IN_PROGRESS = 'IN_PROGRESS',
-  GRANTED = 'GRANTED',
-  DENIED = 'DENIED',
-}
--- src/hooks/sdk/useDevicePermissionStatus.tsx
+++ src/hooks/sdk/useDevicePermissionStatus.tsx
@@ -1,12 +1,12 @@
-// Copyright 2020-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 import { useEffect, useState } from 'react';
-import { useMeetingManager } from 'amazon-chime-sdk-component-library-react';
 
-import { DevicePermissionStatus } from '../enums';
+import { useMeetingManager } from '../../providers/MeetingProvider';
+import { DevicePermissionStatus } from '../../types';
 
-export default function useDevicePermissionStatus() {
+export function useDevicePermissionStatus() {
   const meetingManager = useMeetingManager();
   const [permission, setPermission] = useState<string>(
     DevicePermissionStatus.UNSET
@@ -24,3 +24,5 @@ export default function useDevicePermissionStatus() {
 
   return permission;
 }
+
+export default useDevicePermissionStatus;
