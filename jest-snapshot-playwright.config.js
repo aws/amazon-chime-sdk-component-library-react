ee22391c215119b96381facef5f8879ee7dd95f5
Brad McKnight <63013154+bradmanAWS@users.noreply.github.com>
2021-01-19 10:53:48 -0800

    Remove playwright from library, not used (#378)

--- jest-snapshot-playwright.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'jest-playwright-preset',
-  testRegex: './*\\.test\\.tsx$',
-  setupFilesAfterEnv: ['./tst/setupTests.js'],
-  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx", "node",],
-  testEnvironment: 'jsdom',
-};
