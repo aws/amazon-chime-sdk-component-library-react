7e8e40ecd0e16fecc3ba3a9f7fde5e10a98f8722
Richard Newman <richnew@amazon.com>
2021-02-28 17:36:32 -0800

    Housekeeping.
    This commit:
    
    * Updates a bunch of components, including React.
    * Fixes the incorrect type definitions in date formatting.
    * Corrects the detection of 'yesterday' in said code.
    * Fixes the Jest configuration to not use the deprecated 'tsConfig' spelling of 'tsconfig'.
    * Wraps actions in tests with 'act', as React requests.
    * Adds some key properties to 'ModalButtonGroup', which quiets some React warnings.
    
    Tests pass, though I have not checked the behavior of any demo apps or storybooks.

--- tst/components/ui/PopOver/index.test.tsx
+++ tst/components/ui/PopOver/index.test.tsx
@@ -12,8 +12,15 @@ import lightTheme from '../../../../src/theme/light';
 import { renderWithTheme } from '../../../test-helpers';
 import PopOverItem from '../../../../src/components/ui/PopOver/PopOverItem';
 import PopOverSubMenu from '../../../../src/components/ui/PopOver/PopOverSubMenu';
+import { act } from '@testing-library/react';
 
 let mockFunction: any, value: string, label: string;
+
+function click(it: HTMLElement) {
+  act(() => {
+    fireEvent.click(it);
+  });
+}
 
 describe('PopOver', () => {
   beforeEach(() => {
@@ -92,7 +99,7 @@ describe('PopOver', () => {
     );
     const { getByTestId } = renderWithTheme(lightTheme, component);
     const toggle = getByTestId('popover-toggle');
-    fireEvent.click(toggle);
+    click(toggle);
     const menu = getByTestId('menu');
     await waitFor(() => expect(menu).toBeInTheDocument());
   });
@@ -110,9 +117,9 @@ describe('PopOver', () => {
       component
     );
     const toggle = getByTestId('popover-toggle');
-    fireEvent.click(toggle);
+    click(toggle);
     const menu = queryByTestId('menu');
-    fireEvent.click(toggle);
+    click(toggle);
     expect(menu).not.toBeInTheDocument();
   });
 
@@ -127,10 +134,10 @@ describe('PopOver', () => {
       component
     );
     const toggle = getByTestId('popover-toggle');
-    fireEvent.click(toggle);
+    click(toggle);
     const menu = queryByTestId('menu');
     const item = getByText('Test Item');
-    fireEvent.click(item);
+    click(item);
     expect(menu).not.toBeInTheDocument();
   });
 
@@ -149,10 +156,10 @@ describe('PopOver', () => {
       component
     );
     const toggle = getByTestId('popover-toggle');
-    fireEvent.click(toggle);
+    click(toggle);
     const menu = queryByTestId('menu');
     const item = getByText('Test Item');
-    fireEvent.click(item);
+    click(item);
     expect(menu).toBeInTheDocument();
   });
 
@@ -170,10 +177,10 @@ describe('PopOver', () => {
       component
     );
     const toggle = getByTestId('popover-toggle');
-    fireEvent.click(toggle);
+    click(toggle);
     const menu = queryByTestId('menu');
     const item = getByText('Submenu Item');
-    fireEvent.click(item);
+    click(item);
     await waitFor(() => expect(menu).toBeInTheDocument());
   });
 
@@ -195,7 +202,7 @@ describe('PopOver', () => {
     );
     const { getByTestId } = renderWithTheme(lightTheme, component);
     const toggle = getByTestId('popover-toggle');
-    fireEvent.click(toggle);
+    click(toggle);
     const option2 = getByTestId('option 2');
     userEvent.tab();
     userEvent.tab();
@@ -224,18 +231,20 @@ describe('PopOver', () => {
     );
     const { getByTestId } = renderWithTheme(lightTheme, component);
     const toggle = getByTestId('popover-toggle');
-    fireEvent.click(toggle);
+    click(toggle);
     const option2 = getByTestId('option 2');
-    Simulate.keyDown(document.activeElement || document.body, {
-      key: 'ArrowDown',
-      keyCode: 40,
-      which: 40,
-    }); // key down to Option 1
-    Simulate.keyDown(document.activeElement || document.body, {
-      key: 'ArrowDown',
-      keyCode: 40,
-      which: 40,
-    }); // key down to Option 2
+    act(() => {
+      Simulate.keyDown(document.activeElement || document.body, {
+        key: 'ArrowDown',
+        keyCode: 40,
+        which: 40,
+      }); // key down to Option 1
+      Simulate.keyDown(document.activeElement || document.body, {
+        key: 'ArrowDown',
+        keyCode: 40,
+        which: 40,
+      }); // key down to Option 2
+    });
     await waitFor(() => expect(option2).toHaveFocus());
   });
 
@@ -257,23 +266,25 @@ describe('PopOver', () => {
     );
     const { getByTestId } = renderWithTheme(lightTheme, component);
     const toggle = getByTestId('popover-toggle');
-    fireEvent.click(toggle);
+    click(toggle);
     const option1 = getByTestId('option 1');
-    Simulate.keyDown(document.activeElement || document.body, {
-      key: 'ArrowDown',
-      keyCode: 40,
-      which: 40,
-    }); // key down to Option 1
-    Simulate.keyDown(document.activeElement || document.body, {
-      key: 'ArrowDown',
-      keyCode: 40,
-      which: 40,
-    }); // key down to Option 2
-    Simulate.keyDown(document.activeElement || document.body, {
-      key: 'ArrowUp',
-      keyCode: 38,
-      which: 38,
-    }); // key up to Option 1
+    act(() => {
+      Simulate.keyDown(document.activeElement || document.body, {
+        key: 'ArrowDown',
+        keyCode: 40,
+        which: 40,
+      }); // key down to Option 1
+      Simulate.keyDown(document.activeElement || document.body, {
+        key: 'ArrowDown',
+        keyCode: 40,
+        which: 40,
+      }); // key down to Option 2
+      Simulate.keyDown(document.activeElement || document.body, {
+        key: 'ArrowUp',
+        keyCode: 38,
+        which: 38,
+      }); // key up to Option 1
+    });
     await waitFor(() => expect(option1).toHaveFocus());
   });
 
@@ -290,12 +301,14 @@ describe('PopOver', () => {
       component
     );
     const toggle = getByTestId('popover-toggle');
-    fireEvent.click(toggle);
+    click(toggle);
     const menu = queryByTestId('menu');
-    Simulate.keyDown(document.activeElement || document.body, {
-      key: 'Escape',
-      keyCode: 27,
-      which: 27,
+    act(() => {
+      Simulate.keyDown(document.activeElement || document.body, {
+        key: 'Escape',
+        keyCode: 27,
+        which: 27,
+      });
     });
     await waitFor(() => expect(menu).not.toBeInTheDocument());
   });
