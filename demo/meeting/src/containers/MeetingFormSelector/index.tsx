96dce2e07b4a6ed02e3e8988fde900e5b032ca27
Dmytro Borysovskyi <bbbara10@gmail.com>
2021-04-21 13:16:41 +0300

    Comment 'SIP' button

--- demo/meeting/src/containers/MeetingFormSelector/index.tsx
+++ demo/meeting/src/containers/MeetingFormSelector/index.tsx
@@ -27,9 +27,9 @@ const MeetingFormSelector: React.FC = () => {
   return (
     <StyledWrapper>
       <StyledDiv>{formToShow}</StyledDiv>
-      <Flex container layout="fill-space-centered" style={{ padding: '2rem' }}>
-        <SecondaryButton label={buttonText} onClick={toggle} />
-      </Flex>
+      {/*<Flex container layout="fill-space-centered" style={{ padding: '2rem' }}>*/}
+      {/*  <SecondaryButton label={buttonText} onClick={toggle} />*/}
+      {/*</Flex>*/}
     </StyledWrapper>
   );
 };
