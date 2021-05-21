b21cf7886147fdab8185d95aef84e24a951daa02
Venkatesh Devale <devalevd@amazon.com>
2021-05-20 23:39:35 -0700

    Fix post logging and add corresponding how-to documentation

--- demo/meeting/src/meetingConfig.ts
+++ demo/meeting/src/meetingConfig.ts
@@ -7,12 +7,20 @@ const urlParams = new URLSearchParams(window.location.search);
 const queryLogLevel = urlParams.get('logLevel') || 'warn';
 const logLevel = SDK_LOG_LEVELS[queryLogLevel] || SDK_LOG_LEVELS.warn;
 
+
+const BASE_URL: string = [
+  location.protocol,
+  '//',
+  location.host,
+  location.pathname.replace(/\/*$/, '/'),
+].join('');
+
 const postLogConfig = {
   name: 'SDK_LOGS',
   batchSize: 85,
   intervalMs: 2000,
-  url: '/logs',
-  logLevel
+  url: `${BASE_URL}logs`,
+  logLevel: SDK_LOG_LEVELS.info
 };
 
 const config = {
