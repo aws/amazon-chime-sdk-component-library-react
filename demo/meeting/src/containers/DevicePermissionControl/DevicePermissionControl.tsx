543d790605da4d7f3d821166439899b42b165c57
Sichao Xue <sichax@amazon.com>
2021-06-28 15:32:00 -0700

    Add how-to doc for view-only mode and format code

--- demo/meeting/src/containers/DevicePermissionControl/DevicePermissionControl.tsx
+++ demo/meeting/src/containers/DevicePermissionControl/DevicePermissionControl.tsx
@@ -1,34 +1,54 @@
 // Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { ControlBarButton, Cog, useMeetingManager, Camera, Sound, Dots, DeviceLabels, } from 'amazon-chime-sdk-component-library-react';
+import {
+  ControlBarButton,
+  Cog,
+  useMeetingManager,
+  Camera,
+  Sound,
+  Dots,
+  DeviceLabels,
+} from 'amazon-chime-sdk-component-library-react';
 import React from 'react';
 import DevicePermissionPrompt from '../DevicePermissionPrompt';
 
 const DevicePermissionControl = (props: { deviceLabels: DeviceLabels }) => {
   const meetingManager = useMeetingManager();
+  const { deviceLabels } = props;
 
-  const handleClick = async () => {
-    await meetingManager.invokeDeviceProvider(props.deviceLabels);
+  const handleClick = (): void => {
+    meetingManager.invokeDeviceProvider(deviceLabels);
+  };
+
+  let label;
+  if (deviceLabels === DeviceLabels.AudioAndVideo) {
+    label = 'Device';
+  } else if (deviceLabels === DeviceLabels.Audio) {
+    label = 'Audio';
+  } else if (deviceLabels === DeviceLabels.Video) {
+    label = 'Video';
+  } else {
+    label = 'None';
   }
 
-  const label = props.deviceLabels === DeviceLabels.AudioAndVideo ? "Device" :
-    props.deviceLabels === DeviceLabels.Audio ? "Audio" :
-      props.deviceLabels === DeviceLabels.Video ? "Video" :
-        "None"
+  let icon;
+  if (deviceLabels === DeviceLabels.AudioAndVideo) {
+    icon = <Cog />;
+  } else if (deviceLabels === DeviceLabels.Audio) {
+    icon = <Sound />;
+  } else if (deviceLabels === DeviceLabels.Video) {
+    icon = <Camera />;
+  } else {
+    icon = <Dots />;
+  }
 
-  const icon = props.deviceLabels === DeviceLabels.AudioAndVideo ? <Cog /> :
-    props.deviceLabels === DeviceLabels.Audio ? <Sound /> :
-      props.deviceLabels === DeviceLabels.Video ? <Camera /> :
-        <Dots />
-
-  return (
-    props.deviceLabels === DeviceLabels.None ? null :
-      <>
-        <ControlBarButton icon={icon} onClick={handleClick} label={label} />
-        <DevicePermissionPrompt />
-      </>
+  return deviceLabels === DeviceLabels.None ? null : (
+    <>
+      <ControlBarButton icon={icon} onClick={handleClick} label={label} />
+      <DevicePermissionPrompt />
+    </>
   );
-}
+};
 
 export default DevicePermissionControl;
