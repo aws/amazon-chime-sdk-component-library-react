88482e2f7665dc058a3ab82dfbb3d6d15a628fa0
Venkatesh Devale <devalevd@amazon.com>
2021-09-16 17:27:33 -0700

    Add logger in props to LoggerProvider

--- src/providers/LoggerProvider/index.tsx
+++ src/providers/LoggerProvider/index.tsx
@@ -5,24 +5,40 @@ import React, { useContext } from 'react';
 
 import { ConsoleLogger, Logger, LogLevel } from 'amazon-chime-sdk-js';
 
-export const LoggerContext = React.createContext<Logger|undefined>(undefined);
+export const LoggerContext = React.createContext<Logger | undefined>(undefined);
 
 interface Props {
   /**
    * Represents [LogLevel](https://aws.github.io/amazon-chime-sdk-js/enums/loglevel.html) to determine how verbose the logging should be.
    */
   logLevel?: LogLevel;
-  
+
   /**
    * This name will be used as a prefix when we log statements.
    */
   name?: string;
+
+  /**
+   * Provide a custom [Logger](https://aws.github.io/amazon-chime-sdk-js/interfaces/logger.html).
+   * If not provided, we create and use [ConsoleLogger](https://aws.github.io/amazon-chime-sdk-js/classes/consolelogger.html).
+   * You can also create multiple loggers using [MultiLogger](https://aws.github.io/amazon-chime-sdk-js/classes/multilogger.html).
+   * If provided, the `logLevel` and `name` props will be ignored.
+   */
+  logger?: Logger;
 }
 
-export const LoggerProvider: React.FC<Props> = ({ logLevel = LogLevel.INFO, name = 'ChimeReactComponentSDK', children }) => {
-  const logger: Logger = new ConsoleLogger(name, logLevel);
+export const LoggerProvider: React.FC<Props> = ({ logLevel = LogLevel.INFO, name = 'ChimeSDKReactComponent', logger, children }) => {
+  const createLogger = () => {
+    if (logger) {
+      return logger;
+    } else {
+      return new ConsoleLogger(name, logLevel);
+    }
+  };
+
+  const componentLogger = createLogger();
   return (
-    <LoggerContext.Provider value={logger}>
+    <LoggerContext.Provider value={componentLogger}>
       {children}
     </LoggerContext.Provider>
   )
