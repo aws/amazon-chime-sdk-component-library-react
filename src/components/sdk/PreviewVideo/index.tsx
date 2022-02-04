646ebb84fae00e8552d789ebe5e6c7f0b47bafa5
Michael Hyun <54328451+michhyun1@users.noreply.github.com>
2022-02-04 14:54:50 -0800

    Fix PreviewVideo bug by listening to audioVideo changes to start the Preview (#737)

--- src/components/sdk/PreviewVideo/index.tsx
+++ src/components/sdk/PreviewVideo/index.tsx
@@ -64,7 +64,7 @@ export const PreviewVideo: React.FC<BaseSdkProps> = (props) => {
       }
     }
     startPreview();
-  }, [device]);
+  }, [audioVideo, device]);
 
   return <StyledPreview {...props} ref={videoEl} />;
 };
