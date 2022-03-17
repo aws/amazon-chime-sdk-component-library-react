ed261424e39e2647d496659d55269a1b8700446b
Trung Le <58827450+ltrung@users.noreply.github.com>
2022-03-17 10:33:48 -0700

    Add new release script (#714)

--- scripts/update-sdk-version.js
+++ scripts/update-sdk-version.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const fs = require('fs');
+
+const { currentVersion }  = require('./version-utils');
+
 const {
   logger,
   process,
@@ -7,22 +10,17 @@ const {
   updateDependency
 } = require('./utilities');
 
-// Install the latest version of JS SDK
 process.chdir(path.join(__dirname, '../amazon-chime-sdk/apps/meeting'));
 
 // Remove package-lock to avoid running into fsevents bad platform issue.
 // https://github.com/fsevents/fsevents/issues/336
 // We use Node 16 and it is not stable which breaks the package installation.
 fs.rmSync('./package-lock.json');
-updateDependency('amazon-chime-sdk-js');
 
-// Get the version of the React library tar file
-process.chdir(path.join(__dirname, '..'));
-let package_json = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
-versionString = package_json['version'];
-logger.log(`The current version of React library is ${versionString}`);
+logger.log(`The current version of React library is ${currentVersion}`);
 
 // Pack the latest version of React library and install it in meeting demo
+process.chdir(path.join(__dirname, '..'));
 packDependency();
 process.chdir(path.join(__dirname, '../amazon-chime-sdk/apps/meeting'));
-updateDependency('amazon-chime-sdk-component-library-react', `../../../amazon-chime-sdk-component-library-react-${versionString}.tgz`);
+updateDependency('amazon-chime-sdk-component-library-react', `../../../amazon-chime-sdk-component-library-react-${currentVersion}.tgz`);
