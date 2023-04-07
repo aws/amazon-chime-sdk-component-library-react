e60b43e5f77c266b2cd7f65cd7ffe8e217cd1ca9
Sichao Xue <sichax@amazon.com>
2023-04-07 11:04:19 -0700

    Migrate Radio UI Component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -23,6 +23,8 @@ const config: StorybookConfig = {
     '../src/components/ui/Checkbox/*.stories.tsx',
     '../src/components/ui/FormField/*.mdx',
     '../src/components/ui/FormField/*.stories.tsx',
+    '../src/components/ui/Radio/*.mdx',
+    '../src/components/ui/Radio/*.stories.tsx',
   ],
   addons: [
     {
