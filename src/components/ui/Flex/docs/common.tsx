4b47af394156af6732c76dab4c641ea16d88c0c6
Sichao Xue <sichax@amazon.com>
2023-05-12 16:28:45 -0700

    Add copyright and changelog

--- src/components/ui/Flex/docs/common.tsx
+++ src/components/ui/Flex/docs/common.tsx
@@ -1,4 +1,8 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import styled from 'styled-components';
+
 import Flex from '..';
 
 export const Child = styled(Flex)`
