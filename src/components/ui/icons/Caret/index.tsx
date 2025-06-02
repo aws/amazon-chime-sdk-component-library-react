7b76e4c585663886370299602da4e015cd835d04
Jonathan Wieben <jonathan.wieben@moia.io>
2025-06-02 23:02:11 +0200

    Fix React 19 compatibility issues
    - Update @testing-library/react to 16.3.0 and @testing-library/dom to 10.4.0
    - Fix transform-origin DOM property warnings in SVG components

--- src/components/ui/icons/Caret/index.tsx
+++ src/components/ui/icons/Caret/index.tsx
@@ -33,7 +33,7 @@ export const Caret: React.FC<React.PropsWithChildren<CaretProps>> = ({
   return (
     <StyledCaret direction={direction} {...rest}>
       <path
-        transform-origin="center"
+        style={{ transformOrigin: "center" }}
         d="M8.824 13.88c-.21.18-.526.154-.705-.056-.159-.187-.156-.457-.006-.64l.063-.065 3.523-3c.165-.14.397-.156.577-.05l.074.052 3.477 3c.209.18.232.497.052.706-.16.185-.428.224-.632.104l-.074-.052-3.151-2.72-3.198 2.722z"
       />
     </StyledCaret>
